5c17c64d6722576f296427fc92071c68f8d1078a
maiger <mage_ege@hotmail.com>
2017-01-18 17:06:49 +0200

    Added basics of game states

---
+++ Source/States/Playing.cpp
@@ -0,0 +1,27 @@
+#include "Playing.h"
+
+#include <iostream>
+
+namespace State
+{
+    Playing::Playing(Application& application)
+    : Game_State(application)
+    {
+
+    }
+
+    void Playing::input()
+    {
+
+    }
+
+    void Playing::update()
+    {
+
+    }
+
+    void Playing::draw()
+    {
+        std::cout << "Drawing" << std::endl;
+    }
+}
---
+++ Source/States/Playing.h
@@ -0,0 +1,21 @@
+#ifndef PLAYING_H_INCLUDED
+#define PLAYING_H_INCLUDED
+
+#include "Game_State.h"
+
+class Application;
+
+namespace State
+{
+    class Playing : public Game_State
+    {
+        public:
+            Playing(Application& application);
+
+            void input  () override;
+            void update () override;
+            void draw   () override;
+    };
+}
+
+#endif // PLAYING_H_INCLUDED
