c5c4799a3db43942d57d25e98a65971860948912
maiger <mage_ege@hotmail.com>
2017-01-20 15:05:36 +0200

    Created functions to load shaders

---
+++ Source/Shaders/Shader_Program.cpp
@@ -0,0 +1,27 @@
+#include "Shader_Program.h"
+
+#include "Shader_Loader.h"
+
+namespace Shader
+{
+    Shader_Program::Shader_Program(const std::string& vertexShaderFile, const std::string& fragmentShaderFile)
+    :   m_programID(loadShader(vertexShaderFile, fragmentShaderFile))
+    {
+
+    }
+
+    Shader_Program::~Shader_Program()
+    {
+        glDeleteProgram(m_programID);
+    }
+
+    void Shader_Program::bind()
+    {
+        glUseProgram(m_programID);
+    }
+
+    void Shader_Program::unbind()
+    {
+        glUseProgram(0);
+    }
+}
---
+++ Source/Shaders/Shader_Program.h
@@ -0,0 +1,24 @@
+#ifndef SHADER_PROGRAM_H_INCLUDED
+#define SHADER_PROGRAM_H_INCLUDED
+
+#include <GL/glew.h>
+#include <string>
+
+namespace Shader
+{
+    class Shader_Program
+    {
+        public:
+            Shader_Program(const std::string& vertexShaderFile, const std::string& fragmentShaderFile);
+            ~Shader_Program();
+
+            void bind();
+            void unbind();
+
+        private:
+            GLuint m_programID;
+    };
+}
+
+
+#endif // SHADER_PROGRAM_H_INCLUDED
