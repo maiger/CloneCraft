16282b8c32764a4e79cdd0c32721abec67a065ca
maiger <mage_ege@hotmail.com>
2017-01-23 17:12:04 +0200

    Added support for EBOs

--- Source/States/Playing.cpp
+++ Source/States/Playing.cpp
@@ -4,12 +4,10 @@
 
 std::vector<GLfloat> vertexPositions =
 {
-    0.5, 0.5,
-    -0.5, 0.5,
-    -0.5, -0.5,
-    -0.5, -0.5,
-    0.5, -0.5,
-    0.5, 0.5
+    0.5, 0.5,   // top right
+    -0.5, 0.5,  // top left
+    -0.5, -0.5, // bottom left
+    0.5, -0.5,  // bottom right
 };
 
 std::vector<GLfloat> textureCoords =
@@ -17,17 +15,20 @@ std::vector<GLfloat> textureCoords =
     1.0, 1.0,
     0.0, 1.0,
     0.0, 0.0,
-    0.0, 0.0,
     1.0, 0.0,
-    1.0, 1.0
+};
 
+std::vector<GLuint> indices =
+{
+    0, 1, 2,    // triangle 1
+    2, 3, 0     // triangle 2
 };
 
 namespace State
 {
     Playing::Playing(Application& application)
     : Game_State    (application)
-    , m_model       (vertexPositions, textureCoords)
+    , m_model       (vertexPositions, textureCoords, indices)
     {
         m_texture.load("grass");
     }
@@ -48,7 +49,7 @@ namespace State
         m_model.bind();
         m_texture.bind();
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, m_model.getIndicesCount(), GL_UNSIGNED_INT, nullptr);
 
         m_texture.unbind();
         m_model.unbind();
