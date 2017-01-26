5ceb72f35a01271c8c417e4316e2dad578979325
maiger <mage_ege@hotmail.com>
2017-01-26 15:52:50 +0200

    Going 3D! Bunch of matrices
    Added view, model and projection matrices for 3D stuff

--- Source/Display.h
+++ Source/Display.h
@@ -3,6 +3,9 @@
 
 namespace Display
 {
+    constexpr static int WIDTH = 1280;
+    constexpr static int HEIGHT = 720;
+
     void init();
     void close();
 
--- Source/Renderer/Simple.h
+++ Source/Renderer/Simple.h
@@ -7,6 +7,7 @@
 #include "../Shaders/Simple_Shader.h"
 
 class Quad;
+struct Entity;
 
 namespace Renderer
 {
@@ -15,7 +16,7 @@ namespace Renderer
         public:
             void draw(const Quad& quad);
 
-            void update();
+            void update(const Entity& camera);
 
         private:
             void prepare(const  Quad& quad);
@@ -24,7 +25,6 @@ namespace Renderer
             std::vector<const Quad*> m_quads;
 
             Shader::Simple_Shader m_shader;
-
             sf::Clock m_clock;
     };
 }
